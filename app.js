0b067e2662fe97591288b7e02d5cd7bdbb07a210
timtatt <tim@tattsy.com>
2018-07-11 13:40:01 +1000

    ENHANCE: Add function to register device form

--- app.js
+++ app.js
@@ -21,5 +21,25 @@
 		devices.forEach(function(device) {
 			table.find('tbody').append(`<tr><td>${device.user}</td><td>${device.name}</td></tr>`);
 		});
+
+		_events()
 	});
+
+	function _events() {
+		$('#add-device').on('click', events.onClick.addDevice);
+	}
+
+	var events = {
+		onClick: {
+			addDevice: function(event) {
+				var user = $('#user');
+				var name = $('#name');
+				devices.push({
+					user: user.val(),
+					name: name.val(),
+				});
+				console.log(devices);
+			},
+		}
+	};
 })(jQuery);
